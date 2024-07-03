8e359b73dd9a9054a81af22e8297a5cacd1af8bd
Hokage_Z <132206805+HokageZ@users.noreply.github.com>
2024-07-03 21:48:56 +0300

    Update agents.py

--- bot-builder-crew/agents.py
+++ bot-builder-crew/agents.py
@@ -9,6 +9,7 @@ class BotAgents():
 		return Agent(
 			role='Senior Software Engineer',
 			goal='Create software as needed',
+			tools=self.tools,
 			backstory=dedent("""\
 				You are a Senior Software Engineer at a leading tech think tank.
 				Your expertise in programming in python. and do your best to
@@ -22,14 +23,15 @@ class BotAgents():
 	def qa_engineer_agent(self):
 		return Agent(
 			role='Software Quality Control Engineer',
-  		goal='create prefect code, by analizing the code that is given for errors',
-  		backstory=dedent("""\
+  			goal='create prefect code, by analizing the code that is given for errors',
+  			tools=self.tools,
+     		backstory=dedent("""\
 				You are a software engineer that specializes in checking code
-  			for errors. You have an eye for detail and a knack for finding
+  				for errors. You have an eye for detail and a knack for finding
 				hidden bugs.
-  			You check for missing imports, variable declarations, mismatched
+  				You check for missing imports, variable declarations, mismatched
 				brackets and syntax errors.
-  			You also check for security vulnerabilities, and logic errors"""),
+  				You also check for security vulnerabilities, and logic errors"""),
 			allow_delegation=False,
 			verbose=True,			
    			llm = self.llm
@@ -39,8 +41,9 @@ class BotAgents():
 	def chief_qa_engineer_agent(self):
 		return Agent(
 			role='Chief Software Quality Control Engineer',
-  		goal='Ensure that the code does the job that it is supposed to do',
-  		backstory=dedent("""\
+  			goal='Ensure that the code does the job that it is supposed to do',
+     		tools=self.tools,
+  			backstory=dedent("""\
 				You feel that programmers always do only half the job, so you are
 				super dedicate to make high quality code."""),
 			allow_delegation=True,
